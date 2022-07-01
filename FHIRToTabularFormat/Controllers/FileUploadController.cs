39186ba2d6e55395f24f27cf085feb47b141fe00
Obaid <m.obaidaamir@gmail.com>
2022-07-01 15:11:18 +0100

    Implemented file upload and created Patient model

---
+++ FHIRToTabularFormat/Controllers/FileUploadController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FHIRToTabularFormat.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> files) 
+        {
+            var size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+
+            foreach (var file in files) 
+            {
+                // File Exists
+                if (file.Length > 0) 
+                {
+                    // Create a path 
+                    var filePath = Path.Combine($"{Directory.GetCurrentDirectory()}/wwwroot/data", file.FileName);
+                    filePaths.Add(filePath);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create)) 
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return Ok(new { files.Count, size, filePaths});
+        }
+    }
+}
---
+++ FHIRToTabularFormat/Models/Patient.cs
@@ -0,0 +1,45 @@
+﻿namespace FHIRToTabularFormat.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Prefix { get; set; }
+
+        // There may be more than one contact option, but there will always be at least one. 
+        public List<Dictionary<string, string>> Telecom { get; set; } = new List<Dictionary<string, string>>()
+        {
+            new Dictionary<string, string>()
+            {
+                { "system", null},
+                { "value", null},
+                { "use", null}
+            }
+        };
+
+        public string Gender { get; set; }
+        // year, month, day
+        public DateTime DOB { get; set; } 
+        // year, month, day, hour, min, seconds, UTC timezone
+        public DateTime DeceasedDateTime { get; set; }
+
+        public List<Dictionary<string, string>> Address { get; set; } = new List<Dictionary<string, string>>()
+        {
+            new Dictionary<string, string>()
+            {
+                { "line", null}, // can be more than one, hence need to be addressed properly
+                { "city", null},
+                { "state", null},
+                { "country", null}
+            }
+        };
+
+        public char MaritalStatus { get; set; }
+
+        public bool MultipleBirth { get; set; }
+
+        // List of languages 
+        public List<string> Communication { get; set; }
+
+    }
+}
